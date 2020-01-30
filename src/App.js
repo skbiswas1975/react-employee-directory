a3375f57f444f258f02c86a02d352b60a8ff28b0
skbiswas1975 <you@example.comsbiswas1975@gmail.com>
2020-01-30 22:45:58 +0530

    Employee details section design added

--- src/App.js
+++ src/App.js
@@ -25,37 +25,47 @@ class EmpSearch extends React.Component {
 
     return (
 
-      <div class="container">
-      <h2>Employee Directory</h2>
-      <p>Search with employee name or part of the name in the box bellow:</p>
+      <div class="container-fluid">
+        <div class="col-sm-6">
+      
+            <h2>Employee Directory</h2>
+            <p>Search with employee name or part of the name in the box bellow:</p>
 
-      <div class="form-group">
-        <label for="usr"><h4>Employee name:</h4></label>
-        <input type="text" class="form-control" id="usr" value={filter} onChange={this.handleChange} />
+            <div class="form-group">
+              <label for="usr"><h4>Employee name:</h4></label>
+              <input type="text" class="form-control" id="usr" value={filter} onChange={this.handleChange} />
 
-        <h2>Search result</h2>
-        <p>Click on the employee name for the details:</p>
+              <h2>Search result</h2>
+              <p>Click on the employee name for the details:</p>
 
-        <table class="table table-dark table-hover">
-          <thead>
-            <tr>
-              <th>Employee name</th>
-              <th>Designation</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-          {filteredData.map(item => (
-            <tr>
-              <td>{item.fname} {item.lname}</td>
-              <td>{item.title}</td>
-              <td>{item.email}</td>
-            </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+              <table class="table table-dark table-hover">
+                <thead>
+                  <tr>
+                    <th>Employee name</th>
+                    <th>Designation</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {filteredData.map(item => (
+                  <tr>
+                    <td>{item.fname} {item.lname}</td>
+                    <td>{item.title}</td>
+                    <td>{item.email}</td>
+                  </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
 
+        </div>
+        <div class="col-sm-1">
+                  
+        </div>
+        <div class="col-sm-5">
+          <h2>Employee details</h2>
+          <p>Here is the details of the selected employee:</p>
+        </div>
       </div>
 
     );
