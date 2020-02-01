6b650c326a961a56d5075240d2db375a6e3e9a7e
sanjib biswas <sanjib_biswas2002@MacBook-Pro.local>
2020-02-01 13:08:58 -0600

    Updates

---
+++ precache-manifest.3e3fe3f0cab208e587007b8171c636dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c18c4d120f9fad8abb2f087e1b85d45",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "719d62e871f24e18a3b8",
+    "url": "/react-employee-directory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba525a19d4030cc5e3ba",
+    "url": "/react-employee-directory/static/js/2.6d098c5d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-employee-directory/static/js/2.6d098c5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "719d62e871f24e18a3b8",
+    "url": "/react-employee-directory/static/js/main.6c64d8dd.chunk.js"
+  },
+  {
+    "revision": "32685ed6ab4f63b19564",
+    "url": "/react-employee-directory/static/js/runtime-main.a2fdcd86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
